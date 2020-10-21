13d61befaafe19f6edfd9f8dabcef2cb312970e8
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-21 17:18:23 +0200

    Add links to Footer component and style 'a' element

--- src/components/organisms/footer/Footer.js
+++ src/components/organisms/footer/Footer.js
@@ -4,7 +4,12 @@ import './style.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <span className="copyright">Made by Weronika Olejniczak</span>
+            <span className="copyright">
+                Made by <a href="https://github.com/weronikaolejniczak" alt="Weronika Olejniczak GitHub account" target="_blank" rel="noopener noreferrer">Weronika Olejniczak</a>
+            </span>
+            <span className="repository">
+                Check out GitHub repository at <a href="https://github.com/weronikaolejniczak/colors-game" alt="Colors Game repository at GitHub" target="_blank" rel="noopener noreferrer">@weronikaolejniczak/colors-game</a>
+            </span>
         </footer>
     );
 }
