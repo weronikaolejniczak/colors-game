9c3fb892f3de8a3ba90319d14f28496de5c87f06
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-21 17:44:46 +0200

    Add comments to update board action

--- src/redux/board/boardActions.js
+++ src/redux/board/boardActions.js
@@ -1,5 +1,6 @@
 import {SET_BOARD, UPDATE_BOARD} from './boardTypes';
 import {initializeBoard} from '../../utilities/initializeBoard';
+import {startRound} from '../../utilities/startRound';
 
 export const setBoard = () => {
     const board = initializeBoard();
@@ -13,10 +14,20 @@ export const setBoard = () => {
  * TODO
  * complete updateBoard action
  */
-export const updateBoard = () => {
-    const board = []
+export const updateBoard = (currentBoard, blockCoordinates) => {
+    console.log('currentBoard', currentBoard);
+    console.log('blockCoordinates', blockCoordinates);
+
+    // get blocks that are neighboring and of the same color using a utility for starting a round and checking the surroundings of the given block
+    const blocks = startRound(currentBoard, blockCoordinates[0], blockCoordinates[1]);
+    console.log('blocks', blocks);
+
+    // change values at given coordinates to 0
+    // for each column filter out 0's
+    // generate values for column.length until the BOARD_Y
+
     return {
         type: UPDATE_BOARD,
-        payload: board
+        payload: currentBoard
     }
 }
