90ab8da928ba8b405c43fa5986ae0ec2a4e0d118
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-23 18:02:00 +0200

    Add to do comment to Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,9 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
+/**
+ * TODO: distinguish middleware between production and development
+ */
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
