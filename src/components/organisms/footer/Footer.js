4ac528ad6ae0640d3b8a8290d6043ec779f9f597
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-23 20:33:07 +0200

    Style Header and Footer component

--- src/components/organisms/footer/Footer.js
+++ src/components/organisms/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                 Made by <a href="https://github.com/weronikaolejniczak" alt="Weronika Olejniczak GitHub account" target="_blank" rel="noopener noreferrer">Weronika Olejniczak</a>
             </span>
             <span className="repository">
-                Check out GitHub repository at <a href="https://github.com/weronikaolejniczak/colors-game" alt="Colors Game repository at GitHub" target="_blank" rel="noopener noreferrer">@weronikaolejniczak/colors-game</a>
+                GitHub repository <a href="https://github.com/weronikaolejniczak/colors-game" alt="Colors Game repository at GitHub" target="_blank" rel="noopener noreferrer">@weronikaolejniczak/colors-game</a>
             </span>
         </footer>
     );
--- src/components/organisms/header/Header.js
+++ src/components/organisms/header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {ReactComponent as Logo} from '../../../assets/images/logo.svg';
 import './style.css';
 
 const Header = () => {
     return (
         <header className="header">
-            <span className="logo">Colors Game</span>
+            <div className="logo">
+                <Logo width={100} height={50} />
+            </div>
+            <span className="quote">
+                <span className="quotation-mark">"</span>
+                    With color one obtains an energy that seems to stem from witchcraft.
+                <span className="quotation-mark">" ~</span>
+                    Henri Matisse
+            </span>
         </header>
     );
 }
