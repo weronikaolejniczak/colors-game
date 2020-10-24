f12254f59ce83336d29396f04184452a28c0f85f
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-24 20:59:44 +0200

    Add comments and comment out one color

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import Colors from './data/Colors';
 
+/* change the dimensions of the board here */
 export const BOARD_X = 12;
 export const BOARD_Y = 12;
 export const COLORS = Object.keys(Colors).length;
--- src/data/Colors.js
+++ src/data/Colors.js
@@ -1,11 +1,12 @@
 const Colors = {
-    1: '#ff602f',
+    1: '#6fc6b2',
     2: '#fded73',
     3: '#7250dd',
     4: '#e67fc4',
-    5: '#6fc6b2',
+    5: '#297ef0',
     6: '#ffa311',
-    7: '#297ef0'
+    /* 7: 'ff602f' */
+    /* add more colors here */
 }
 
 export default Colors;
