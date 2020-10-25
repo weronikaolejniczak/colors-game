39fd35315860bda794244d0eec5d6f304db18793
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-25 13:35:03 +0100

    Updates

---
+++ precache-manifest.ebd6b15833631fc1b28610d611d7f344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72002a7d47833246736474dee45f1142",
+    "url": "/colors-game/index.html"
+  },
+  {
+    "revision": "a103140141f1cd5c561b",
+    "url": "/colors-game/static/css/main.462bbf56.chunk.css"
+  },
+  {
+    "revision": "5eb7cf9f3e9a1ed77806",
+    "url": "/colors-game/static/js/2.ea8a1cb0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/colors-game/static/js/2.ea8a1cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a103140141f1cd5c561b",
+    "url": "/colors-game/static/js/main.82dc8384.chunk.js"
+  },
+  {
+    "revision": "f9ae25b0f5e0ee76ad95",
+    "url": "/colors-game/static/js/runtime-main.872c81b9.js"
+  },
+  {
+    "revision": "6e4480ef5c10071943bbc40dbcb8a91c",
+    "url": "/colors-game/static/media/logo.6e4480ef.svg"
+  }
+]);
